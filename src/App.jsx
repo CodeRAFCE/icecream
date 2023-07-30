8e36b0d7df6cb8ad276f66e94dad8260f6460df1
CodeRAFCE <seshan.io@prismscale.com>
2023-07-30 19:00:30 +0530

    UI preparations for the validations

---
+++ src/App.jsx
@@ -0,0 +1,132 @@
+import {useState} from "react";
+import CustomDropdown from "./components/CustomDropdown";
+
+const COUNT = 1;
+
+const COLOR_OPTIONS = [
+	{color: "red", name: "RED", id: 1, icon: ""},
+	{color: "orange", name: "ORANGE", id: 2, icon: ""},
+	{color: "yellow", name: "YELLOW", id: 3, icon: ""},
+	{color: "green", name: "GREEN", id: 4, icon: ""},
+	{color: "blue", name: "BLUE", id: 5, icon: ""},
+	{color: "indigo", name: "INDIGO", id: 6, icon: ""},
+	{color: "violet", name: "VIOLET", id: 7, icon: ""},
+];
+
+function App() {
+	const [empId, setEmpId] = useState();
+	const [uid, setUid] = useState();
+	const [token, setToken] = useState(COUNT);
+	const [selectedColor, setSelectedColor] = useState("");
+	// const [isSubmitted, setIsSubmitted] = useState(false);
+
+	// * --------------------------------------------------------
+	// * FORM HANDLER METHODS
+
+	const onEmpIdChange = (event) => {
+		const employeeId = event.target.value;
+		setUid(employeeId);
+	};
+
+	const onUidChange = (event) => {
+		const uniqueId = event.target.value;
+		setEmpId(uniqueId);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// setIsSubmitted(true);
+
+		const data = {
+			empId,
+			uid,
+			token,
+			selectedColor,
+		};
+
+		console.log("SUBMIT CLICKED", data);
+
+		setToken((prev) => prev + 1);
+	};
+	// * --------------------------------------------------------
+
+	return (
+		<>
+			<header className="h-20 w-full mb-10">
+				<nav className="max-w-7xl h-full mx-auto px-6 flex items-center justify-between">
+					<div>LOGO1</div>
+					<div>LOGO2</div>
+				</nav>
+			</header>
+
+			<main className="max-w-7xl mx-auto px-6">
+				<section>
+					<form className="max-w-xl mx-auto" onSubmit={handleSubmit}>
+						<div className="space-y-4">
+							<fieldset>
+								<label htmlFor="empId" className="font-semibold uppercase">
+									Employee ID
+								</label>
+								<input
+									name="empId"
+									id="empId"
+									type="text"
+									onChange={onEmpIdChange}
+									placeholder="Enter Employee ID"
+									className="border border-slate-500 focus:outline-none px-2 py-2 w-full rounded"
+									required
+								/>
+							</fieldset>
+
+							{/* ------------------------------------------------------------------ */}
+
+							<fieldset>
+								<label htmlFor="uid" className="font-semibold uppercase">
+									UID
+								</label>
+								<input
+									name="uid"
+									id="uid"
+									type="text"
+									onChange={onUidChange}
+									placeholder="Enter a unique id"
+									className="border border-slate-500 focus:outline-none px-2 py-2 w-full rounded"
+									required
+								/>
+							</fieldset>
+
+							<fieldset>
+								<label htmlFor="colors" className="font-semibold uppercase">
+									colors
+								</label>
+
+								<CustomDropdown options={COLOR_OPTIONS} onChange={setSelectedColor} />
+							</fieldset>
+						</div>
+
+						<div className="p-8 text-center bg-white border border-zinc-300 rounded my-10">
+							<h1 className="text-2xl md:text-5xl font-semibold">TOKEN: {token}</h1>
+						</div>
+
+						<div className="flex flex-row gap-3 justify-center">
+							<button
+								type="submit"
+								className="uppercase px-6 py-2 bg-orange-400 text-white rounded"
+							>
+								Send
+							</button>
+							<button
+								type="submit"
+								className="uppercase px-6 py-2 bg-orange-400 text-white rounded"
+							>
+								GO to EXCEL
+							</button>
+						</div>
+					</form>
+				</section>
+			</main>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/CustomDropdown.jsx
@@ -0,0 +1,72 @@
+import PropTypes from "prop-types";
+import {useState} from "react";
+
+const CustomDropdown = ({options, onChange}) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [selectedOption, setSelectedOption] = useState(options[0]);
+
+	const toggleDropdown = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleClose = () => {
+		setIsOpen(false);
+	};
+
+	const handleOptionClick = (option) => {
+		setSelectedOption(option);
+		setIsOpen(false);
+		onChange(option.name); // Passing the selected value to the parent component
+	};
+
+	return (
+		<>
+			{isOpen ? (
+				<div className="absolute inset-0 bg-black opacity-0 z-10" onClick={handleClose} />
+			) : null}
+
+			<div className="relative">
+				<div
+					onClick={toggleDropdown}
+					className="cursor-pointer bg-white border border-slate-500 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+				>
+					{selectedOption.name}
+					<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+						<svg
+							className={`fill-current h-4 w-4 ${isOpen ? "transform rotate-180" : ""}`}
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 20 20"
+						>
+							<path d="M14.95 9.95l-4 4c-.2.2-.45.3-.7.3s-.5-.1-.7-.3l-4-4c-.4-.4-.4-1 0-1.4s1-.4 1.4 0L10 11.6l3.6-3.6c.4-.4 1-.4 1.4 0s.4 1 0 1.4z" />
+						</svg>
+					</div>
+				</div>
+				{isOpen && (
+					<div className="mt-2 py-1 bg-white border border-gray-300 rounded shadow-lg absolute z-50 w-full">
+						{options.map((option) => {
+							const {color, name, id} = option;
+							return (
+								<div
+									key={id}
+									onClick={() => handleOptionClick(option)}
+									className={`cursor-pointer py-2 px-4 hover:bg-gray-200 ${
+										selectedOption.color === color ? "bg-gray-100" : ""
+									}`}
+								>
+									{name}
+								</div>
+							);
+						})}
+					</div>
+				)}
+			</div>
+		</>
+	);
+};
+
+CustomDropdown.propTypes = {
+	options: PropTypes.array.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default CustomDropdown;
