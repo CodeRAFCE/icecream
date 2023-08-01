cd2503fad0325dd97e2af529b20e3281e5f39404
CodeRAFCE <seshan.io@prismscale.com>
2023-08-02 01:48:56 +0530

    changing sheetbdId v3

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import MessageStatus from "./components/MessageStatus";
 import ColorCode from "./components/ColorCode";
 
 const AUTHORIZATION_CODE = 7777777;
-const SHEET_URL = `https://sheetdb.io/api/v1/pcd7i3hgpjxpp`;
+const SHEET_URL = `https://sheetdb.io/api/v1/91i4jl9lzr7ok`;
 const MULTI_SHEET_URL = `${SHEET_URL}?sheet=`;
 
 // eslint-disable-next-line no-unused-vars
